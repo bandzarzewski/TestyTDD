a26e4c5b3e6123be960c9d28f3cdd8f63abb6c4a
RENT-Komputer\RENT <m.bandzarzewski@gmail.com>
2017-08-16 17:58:57 +0200

    Vending machine

---
+++ TestyDay3/src/VendingMachineTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class VendingMachineTest {
+    @Test
+
+    public void WhenChooseingProductsWithoutMoneyNotihingHappen(){
+        VendingMachine vendingMachine=new VendingMachine();
+        String result=vendingMachine.execute("A");
+        Assert.fail();
+    }
+}
