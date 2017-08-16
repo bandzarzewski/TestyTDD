810666969843890435beb683839361a72511b7df
RENT-Komputer\RENT <m.bandzarzewski@gmail.com>
2017-08-16 17:44:15 +0200

    Nastepny commit

--- TestyDay3/src/com/company/Main.java
+++ TestyDay3/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Testzxsx");
         // write your code here
     }
 }
